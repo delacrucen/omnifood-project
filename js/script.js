df3ecbc12f5f07953c2464c5d447770a9b43c372
Mauricio De La Cruz <delacruzen@hotmail.com>
2023-02-16 18:28:47 -0300

    Add sticky navigation functionality

--- js/script.js
+++ js/script.js
@@ -53,6 +53,28 @@ allLinks.forEach((link) => {
 });
 
 ///////////////////////////////////////////////////////////
+// Sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+
+// most performant, modern way
+const obs = new IntersectionObserver(
+  (entries) => {
+    const ent = entries[0];
+    console.log(ent);
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    } else {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport - null means the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
